d73ac888244f67f695095acddd667684f4357ede
Munib <107819673+IAMMUNIBMIR@users.noreply.github.com>
2024-06-25 20:19:34 +0300

    Date Display

--- PredictionModel.py
+++ PredictionModel.py
@@ -36,7 +36,8 @@ def get_data(cryptos, currency):
             try:
                 tmp = HistoricalData(pair, 60*60*24, start_date.strftime('%Y-%m-%d-00-00'), (start_date + delta).strftime('%Y-%m-%d-00-00'), verbose=False).retrieve_data()
                 if tmp.empty:
-                    break
+                    start_date += delta
+                    continue
                 coinprices = pd.concat([coinprices, tmp[['close']]], axis=0)  # Concatenate along rows (axis=0)
             except Exception as e:
                 return None, f"Error fetching data for {pair} between {start_date} and {start_date + delta}: {str(e)}"
